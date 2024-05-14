b4d4aed2c31cd921e9e6e856c8302c6193daa2f3
ukorvl <ukorotovskiy@gmail.com>
2024-05-14 16:22:49 +0300

    initialize project #1

---
+++ src/client.ts
@@ -0,0 +1,13 @@
+type ClientConfig = {
+  endpoint: string;
+};
+
+/**
+ * Creates a new client to interact with the network API.
+ */
+const createClient = (config: ClientConfig) => {
+  const client = null;
+  return client;
+};
+
+export { createClient };
