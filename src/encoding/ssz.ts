8edcc5964335b6f53f3ebec2fd536841e26f0ce3
Nick <newbiecraft@gmail.com>
2024-06-10 18:22:13 +0700

    fix #25: remove hack to support zerohashes like on go-side (#27)
    * fix #25: remove hack to support zerohashes like on go-side
    
    * add changeset #25
    
    * chore #25

--- src/encoding/ssz.ts
+++ src/encoding/ssz.ts
@@ -14,42 +14,28 @@ import {
 import { concatBytes } from "@noble/curves/abstract/utils";
 import { poseidonHash } from "./poseidon.js";
 
-// TODO: delete when it will be fixed on backend
-export const initZeroHashes = () => {
-  const testType = new ContainerType({
-    list: new ByteListType(2 ** 32 - 1),
-  });
-
-  testType.hashTreeRoot({
-    list: Uint8Array.from([]),
-  });
-
-  setHasher({
-    digest64(a, b) {
-      // console.log("digest a b", a, b);
-      const hash = poseidonHash(concatBytes(a, b));
-      const arr = new Uint8Array(32);
-      for (let i = 0; i < 32; i++) {
-        // Shift the BigInt to the right by 8 * i bits, then take the 8 least significant bits
-        arr[31 - i] = Number((hash >> BigInt(i * 8)) & BigInt(0xff));
-      }
-      return arr;
-    },
-    digest64HashObjects(a, b) {
-      const hash = poseidonHash(
-        concatBytes(hashObjectToUint8Array(a), hashObjectToUint8Array(b)),
-      );
-      const arr = new Uint8Array(32);
-      for (let i = 0; i < 32; i++) {
-        // Shift the BigInt to the right by 8 * i bits, then take the 8 least significant bits
-        arr[31 - i] = Number((hash >> BigInt(i * 8)) & BigInt(0xff));
-      }
-      return uint8ArrayToHashObject(arr);
-    },
-  });
-};
-
-initZeroHashes();
+setHasher({
+  digest64(a, b) {
+    const hash = poseidonHash(concatBytes(a, b));
+    const arr = new Uint8Array(32);
+    for (let i = 0; i < 32; i++) {
+      // Shift the BigInt to the right by 8 * i bits, then take the 8 least significant bits
+      arr[31 - i] = Number((hash >> BigInt(i * 8)) & BigInt(0xff));
+    }
+    return arr;
+  },
+  digest64HashObjects(a, b) {
+    const hash = poseidonHash(
+      concatBytes(hashObjectToUint8Array(a), hashObjectToUint8Array(b)),
+    );
+    const arr = new Uint8Array(32);
+    for (let i = 0; i < 32; i++) {
+      // Shift the BigInt to the right by 8 * i bits, then take the 8 least significant bits
+      arr[31 - i] = Number((hash >> BigInt(i * 8)) & BigInt(0xff));
+    }
+    return uint8ArrayToHashObject(arr);
+  },
+});
 
 const basicTypes = {
   Uint8: new UintNumberType(1),
