77844810c235fca8842c28f0efa2f925abb2ecde
khannanov-nil <khannanov@nil.foundation>
2024-06-21 11:21:16 +0000

    jsdoc comments added for transports

--- src/transport/MetaMaskTransport.ts
+++ src/transport/MetaMaskTransport.ts
@@ -3,10 +3,35 @@ import invariant from "tiny-invariant";
 import type { IHttpTransportConfig } from "./types/IHttpTransportConfig.js";
 import type { ITransport } from "./types/ITransport.js";
 
+/**
+ * MetaMask transport represents the MetaMask transport for connecting to the network.
+ * MetaMask transport can be used in browser only.
+ * @class MetaMaskTransport
+ * @typedef {MetaMaskTransport}
+ * @implements {ITransport}
+ */
 class MetaMaskTransport implements ITransport {
+  /**
+   * The provider for the transport.
+   *
+   * @private
+   * @type {*}
+   */
   private provider;
+  /**
+   * The request timeout.
+   *
+   * @private
+   * @type {number}
+   */
   private timeout: number;
 
+  /**
+   * Creates an instance of MetaMaskTransport.
+   *
+   * @constructor
+   * @param {IHttpTransportConfig} config The transport config.
+   */
   constructor(config: IHttpTransportConfig) {
     this.timeout = config.timeout !== undefined ? config.timeout : 20000;
 
@@ -23,14 +48,33 @@ class MetaMaskTransport implements ITransport {
     this.provider = window.ethereum;
   }
 
+  /**
+   * Sends a request to the network.
+   *
+   * @public
+   * @async
+   * @template T
+   * @param {RequestArguments} requestObject The request object.
+   * @returns {Promise<T>} The response.
+   */
   public async request<T>(requestObject: RequestArguments): Promise<T> {
     return this.provider.request(requestObject);
   }
 
+  /**
+   * Connects to the network.
+   *
+   * @public
+   */
   public connect(): void {
     this.provider.request({ method: "eth_requestAccounts" });
   }
 
+  /**
+   * Closes the connection to the network.
+   *
+   * @public
+   */
   public closeConnection(): void {
     //
   }
--- src/transport/types/IHttpTransportConfig.ts
+++ src/transport/types/IHttpTransportConfig.ts
@@ -1,14 +1,14 @@
 /**
- * The configuration of the HTTP transport.
+ * The interface representing the configuration of the HTTP transport.
  */
 type IHttpTransportConfig = {
   /**
-   * The endpoint of the network. It is a URL of the network node.
+   * The network endpoint. It is set to the URL of the network node.
    * @example 'http://127.0.0.1:8529'
    */
   endpoint: string;
   /**
-   * The timeout is used to set the timeout for the request.
+   * The request timeout.
    * If the request is not completed within the timeout, it will be rejected.
    * @example 1000
    * @default 20000
