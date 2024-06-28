653d1de6f879908245417002e3d1ef1f81c902b5
klond90 <newbiecraft@gmail.com>
2024-06-28 20:00:26 +0400

    add integration tests #63

---
+++ src/integrations/bounce.test.ts
@@ -0,0 +1,92 @@
+import { bytesToHex, encodeFunctionData } from "viem";
+import { testEnv } from "../../test/testEnv.js";
+import {
+  Faucet,
+  HttpTransport,
+  LocalECDSAKeySigner,
+  PublicClient,
+  WalletV1,
+  convertEthToWei,
+  generateRandomPrivateKey,
+  waitTillCompleted,
+} from "../index.js";
+const client = new PublicClient({
+  transport: new HttpTransport({
+    endpoint: testEnv.endpoint,
+  }),
+  shardId: 1,
+});
+
+test("bounce", async () => {
+  const faucet = new Faucet(client);
+
+  const signer = new LocalECDSAKeySigner({
+    privateKey: generateRandomPrivateKey(),
+  });
+
+  const pubkey = await signer.getPublicKey();
+
+  const wallet = new WalletV1({
+    pubkey: pubkey,
+    salt: 100n,
+    shardId: 1,
+    client,
+    signer,
+  });
+  const walletAddress = await wallet.getAddressHex();
+
+  const anotherWallet = new WalletV1({
+    pubkey: pubkey,
+    salt: 200n,
+    shardId: 1,
+    client,
+    signer,
+  });
+
+  const seqno = await client.getMessageCount(Faucet.address, "latest");
+
+  const faucetHash1 = await faucet.withdrawTo(
+    walletAddress,
+    convertEthToWei(0.1),
+    seqno,
+  );
+  const faucetHash2 = await faucet.withdrawTo(
+    anotherWallet.getAddressHex(),
+    convertEthToWei(0.1),
+    seqno + 1,
+  );
+
+  await waitTillCompleted(client, 1, bytesToHex(faucetHash1));
+  await waitTillCompleted(client, 1, bytesToHex(faucetHash2));
+  await wallet.selfDeploy(true);
+  await anotherWallet.selfDeploy(true);
+
+  const bounceAddress = WalletV1.calculateWalletAddress({
+    pubKey: pubkey,
+    shardId: 1,
+    salt: 300n,
+  });
+
+  const hash = await wallet.sendMessage({
+    to: anotherWallet.getAddressHex(),
+    value: 10_000_000n,
+    bounceTo: bounceAddress,
+    gas: 100_000n,
+    data: encodeFunctionData({
+      abi: WalletV1.abi,
+      functionName: "syncCall",
+      args: [walletAddress, 100_000, 10_000_000, "0x"],
+    }),
+  });
+
+  const receipts = await waitTillCompleted(client, 1, hash);
+
+  expect(receipts.length).toBeDefined();
+  expect(receipts.some((r) => !r.success)).toBe(true);
+
+  expect(receipts.length).toBeGreaterThan(2);
+
+  const balance = await client.getBalance(bytesToHex(bounceAddress), "latest");
+
+  expect(balance).toBeGreaterThan(0n);
+});
---
+++ src/integrations/calling.test.ts
@@ -0,0 +1,110 @@
+import { bytesToHex } from "viem";
+import { testEnv } from "../../test/testEnv.js";
+import {
+  Faucet,
+  HttpTransport,
+  LocalECDSAKeySigner,
+  PublicClient,
+  WalletV1,
+  convertEthToWei,
+  generateRandomPrivateKey,
+  waitTillCompleted,
+} from "../index.js";
+const client = new PublicClient({
+  transport: new HttpTransport({
+    endpoint: testEnv.endpoint,
+  }),
+  shardId: 1,
+});
+
+test("Async call to another shard send value", async () => {
+  const faucet = new Faucet(client);
+
+  const signer = new LocalECDSAKeySigner({
+    privateKey: generateRandomPrivateKey(),
+  });
+
+  const pubkey = await signer.getPublicKey();
+
+  const wallet = new WalletV1({
+    pubkey: pubkey,
+    salt: 100n,
+    shardId: 1,
+    client,
+    signer,
+  });
+  const walletAddress = await wallet.getAddressHex();
+
+  const faucetHash = await faucet.withdrawTo(walletAddress, convertEthToWei(1));
+
+  await waitTillCompleted(client, 1, bytesToHex(faucetHash));
+  await wallet.selfDeploy(true);
+
+  expect(walletAddress).toBeDefined();
+
+  const anotherAddress = WalletV1.calculateWalletAddress({
+    pubKey: pubkey,
+    shardId: 2,
+    salt: 200n,
+  });
+
+  const hash = await wallet.sendMessage({
+    to: anotherAddress,
+    value: 50_000_000n,
+    gas: 100_000n,
+  });
+
+  const receipts = await waitTillCompleted(client, 1, hash);
+
+  expect(receipts).toBeDefined();
+  expect(receipts.some((r) => !r.success)).toBe(false);
+
+  const balance = await client.getBalance(bytesToHex(anotherAddress), "latest");
+  expect(balance).toBeGreaterThan(0n);
+});
+
+test("sync call same shard send value", async () => {
+  const faucet = new Faucet(client);
+
+  const signer = new LocalECDSAKeySigner({
+    privateKey: generateRandomPrivateKey(),
+  });
+
+  const pubkey = await signer.getPublicKey();
+
+  const wallet = new WalletV1({
+    pubkey: pubkey,
+    salt: 100n,
+    shardId: 1,
+    client,
+    signer,
+  });
+  const walletAddress = await wallet.getAddressHex();
+
+  const faucetHash = await faucet.withdrawTo(
+    walletAddress,
+    convertEthToWei(0.1),
+  );
+  await waitTillCompleted(client, 1, bytesToHex(faucetHash));
+  await wallet.selfDeploy(true);
+
+  const anotherAddress = WalletV1.calculateWalletAddress({
+    pubKey: pubkey,
+    shardId: 1,
+    salt: 200n,
+  });
+
+  const hash = await wallet.syncSendMessage({
+    to: anotherAddress,
+    value: 10n,
+    gas: 100000n,
+  });
+
+  const receipts = await waitTillCompleted(client, 1, hash);
+
+  expect(receipts).toBeDefined();
+  expect(receipts.some((r) => !r.success)).toBe(false);
+  const balance = await client.getBalance(bytesToHex(anotherAddress), "latest");
+
+  expect(balance).toBe(10n);
+});
