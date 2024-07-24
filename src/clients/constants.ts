db0d06d90c552e2b252bc9bece159b201589bf8e
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-07-24 15:02:44 +0800

    fix: fix masterShardId being assigned to shard 0

--- src/clients/constants.ts
+++ src/clients/constants.ts
@@ -1,4 +1,5 @@
 const emptyAddress = "0x0000000000000000000000000000000000000000";
-const masterShardId = 0;
+// TODO: as of devnet there is not master shard, this constant should be updated once we have a master shard
+const masterShardId = 2 ** 16 - 1;
 
 export { emptyAddress, masterShardId };
